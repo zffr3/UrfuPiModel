2d830b3afc82a29378ce015c3a23350b6d6c17ea
Zefir3 <chechulin.m@list.ru>
2024-01-17 11:32:59 +0500

    task2 fix

--- Task2/main.py
+++ Task2/main.py
@@ -1,11 +1,10 @@
-from typing import Union
 from fastapi import FastAPI
 from transformers import pipeline
 
 app = FastAPI()
 
 @app.get("/nlp/{text}")
-def analyse_text(text: str, q: Union[str, None]=None):
+def analyse_text(text: str):
     pip = pipeline('sentiment-analysis', 'blanchefort/rubert-base-cased-sentiment')
     out = pip(text)
     return out
